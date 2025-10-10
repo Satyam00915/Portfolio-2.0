c6c7c0c15fe708b7d674c7f32e7bc29def4cceb9
Satyam00915 <satyamupadhyay00915@gmail.com>
2025-10-10 14:04:47 +0530

    Links

--- src/Pages/Hero.tsx
+++ src/Pages/Hero.tsx
@@ -51,7 +51,11 @@ const Hero = () => {
         {/* Social Links */}
         <div className="flex items-center justify-center gap-5">
           {/* GitHub */}
-          <button className="group relative">
+          <a
+            target="_blank"
+            href="https://github.com/Satyam00915"
+            className="group relative"
+          >
             <svg
               strokeLinejoin="round"
               strokeLinecap="round"
@@ -63,10 +67,14 @@ const Hero = () => {
             >
               <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" />
             </svg>
-          </button>
+          </a>
 
           {/* LinkedIn */}
-          <button className="group relative">
+          <a
+            target="_blank"
+            href="https://www.linkedin.com/in/satyam0915/"
+            className="group relative"
+          >
             <svg
               viewBox="0 0 382 382"
               xmlns="http://www.w3.org/2000/svg"
@@ -89,7 +97,7 @@ const Hero = () => {
                    c73.552,0,73.131,68.716,73.131,106.472L341.91,330.654z"
               />
             </svg>
-          </button>
+          </a>
         </div>
 
         {/* Skills */}
